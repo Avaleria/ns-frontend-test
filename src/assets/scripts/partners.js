ad73b5887d2ff71eed78f473a409ed373451530a
Anielka <melarav501@gmail.com>
2019-11-21 01:58:28 -0600

    Make project responsive

--- src/assets/scripts/partners.js
+++ src/assets/scripts/partners.js
@@ -1,38 +1,48 @@
 var partners = [
-    {
-        "name": "Bradley Hunter",
-        "description": "Based in Chicago. I love playing tennis and loud music.",
-        "image": "./img/partners/bradley.png"
-    },
-    {
-        "name": "Marie Bennett",
-        "description": "Currently living in Colorado. Lover of art, languages and travelling.",
-        "image": "./img/partners/marie.png"
-    },
-    {
-        "name": "Diana Wells",
-        "description": "Living in Athens, Greece. I love black and white classics, chillout music and green tea.",
-        "image": "./img/partners/diana.png"
-    },
-    {
-        "name": "Christopher Pierce",
-        "description": "Star Wars fanatic. I have a persistent enthusiasm to create new things.",
-        "image": "./img/partners/cristopher.png"
-    }
+  {
+    name: "Bradley Hunter",
+    description: "Based in Chicago. I love playing tennis and loud music.",
+    image: "./img/partners/bradley.png"
+  },
+  {
+    name: "Marie Bennett",
+    description:
+      "Currently living in Colorado. Lover of art, languages and travelling.",
+    image: "./img/partners/marie.png"
+  },
+  {
+    name: "Diana Wells",
+    description:
+      "Living in Athens, Greece. I love black and white classics, chillout music and green tea.",
+    image: "./img/partners/diana.png"
+  },
+  {
+    name: "Christopher Pierce",
+    description:
+      "Star Wars fanatic. I have a persistent enthusiasm to create new things.",
+    image: "./img/partners/cristopher.png"
+  }
 ];
 
 function loadPartners() {
-    var page = document.getElementById("partner-container");
-    for (var i = 0; i < partners.length; i++) {
-        var partner = partners[i];
-        page
-        .innerHTML += 
-        "<div class=\"partner-content \">\n" +
-            "<img src=\"" + partner.image + "\" class=\"partner-avatar\" alt=\"" + partner.name + "\"/>\n" + 
-            "<span class=\"header-partner\">"+ partner.name + "</span>" +
-            "<span class=\"desc-partner\">"+ partner.description + "</span>" +
-        "</div>";
-    }
+  var page = document.getElementById("partner-container");
+  for (var i = 0; i < partners.length; i++) {
+    var partner = partners[i];
+    page.innerHTML +=
+      '<div class="partner-content container__col-12 container__col-md-6 container__col-lg-3">\n' +
+      '<img src="' +
+      partner.image +
+      '" class="partner-avatar" alt="' +
+      partner.name +
+      '"/>\n' +
+      '<span class="header-partner">' +
+      partner.name +
+      "</span>" +
+      '<span class="desc-partner">' +
+      partner.description +
+      "</span>" +
+      "</div>";
+  }
 }
 
 loadPartners();
--- src/assets/scripts/steps.js
+++ src/assets/scripts/steps.js
@@ -1,37 +1,48 @@
 var steps = [
-    {
-        "id": "STEP 1",
-        "image": "./img/steps/Step1.png",
-        "title": "Sed leo enim, condimentum",
-        "detail": "Quisque libero libero, dictum non turpis in, luctus semper lorem. Donec rhoncus a leo sit amet facilisis."
-    },
-    {
-        "id": "STEP 2",
-        "image": "./img/steps/Step2.png",
-        "title": "Morbi velit risus",
-        "detail": "Nulla venenatis tempor dui in molestie. Nulla quis dictum purus, sit amet porttitor est."
-    },
-    {
-        "id": "STEP 3",
-        "image": "./img/steps/Step3.png",
-        "title": "Sed leo enim, condimentum",
-        "detail": "Quisque libero libero, dictum non turpis in, luctus semper lorem. Donec rhoncus a leo sit amet facilisis."
-    }
+  {
+    id: "STEP 1",
+    image: "./img/steps/Step1.png",
+    title: "Sed leo enim, condimentum",
+    detail:
+      "Quisque libero libero, dictum non turpis in, luctus semper lorem. Donec rhoncus a leo sit amet facilisis."
+  },
+  {
+    id: "STEP 2",
+    image: "./img/steps/Step2.png",
+    title: "Morbi velit risus",
+    detail:
+      "Nulla venenatis tempor dui in molestie. Nulla quis dictum purus, sit amet porttitor est."
+  },
+  {
+    id: "STEP 3",
+    image: "./img/steps/Step3.png",
+    title: "Sed leo enim, condimentum",
+    detail:
+      "Quisque libero libero, dictum non turpis in, luctus semper lorem. Donec rhoncus a leo sit amet facilisis."
+  }
 ];
 function loadSteps() {
-    for (var i = 0; i < steps.length; i++) {
-        var step = steps[i];
-        document.getElementById("step-container")
-        .innerHTML += 
-            "<div class=\"steps\">\n" +
-                "<img src=\"" + step.image + "\" alt=\"" + step.id + "\"/>\n" + 
-                "<div>"+
-                    "<button>" + step.id + "</button>"+
-                    " <span class=\"step-header\">" + step.title + "</span>\n" +
-                    " <span class=\"step-desc\">" + step.detail + "</span>\n" +
-                " </div>"+
-            " </div>";
-    }
+  for (var i = 0; i < steps.length; i++) {
+    console.log(steps);
+    var step = steps[i];
+    console.log(step);
+    document.getElementById("step-container").innerHTML +=
+      '<div class="container__col-12 container__col-md-4 container__col-lg-4">\n' +
+      '<div class="flex-column" style="background: url(' +
+      step.image +
+      ') no-repeat top center; height: 400px; width: 300px;margin: 5px;justify-content:flex-end">\n' +
+      "<button>" +
+      step.id +
+      "</button>" +
+      '<span class="step-header">' +
+      step.title +
+      "</span>" +
+      '<span class="step-desc">' +
+      step.detail +
+      "</span>" +
+      "</div>" +
+      "</div>";
+  }
 }
 
 loadSteps();
